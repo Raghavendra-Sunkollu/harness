f48f102cca006d6e41f62046eabdc5157db9ec4b
Raghavendra-Sunkollu <71206184+Raghavendra-Sunkollu@users.noreply.github.com>
2022-06-27 21:17:06 +0530

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,19 @@
+import math
+
+# n=int(input())
+list=[1,2,2,24]
+a="0980"
+a=int(a)
+# for i in range (0,n):
+#     list.append(input())
+# #print(list)
+# for ele in list:
+#     try:
+#         pattern=re.compile("\d{16}")
+#         p=pattern.findall(ele)
+#         if not p:
+#             print("valid")
+#     except:
+#         print("Invalid exception")
+#
+print("hello world")
